822ad34101c430d6075070f3981d711b2b2d9979
jqp2401 <jqueper2401@g.educaand.es>
2025-10-23 13:12:52 +0200

    v0.2 modificar App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola soy Juan");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Como te llamas?");
+        String nombre = sc.nextLine();
+        System.out.println("Encantado de conocerte " + nombre);
     }
 }
